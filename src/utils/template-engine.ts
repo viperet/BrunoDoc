061c9958b8c10efe66c38ab691bf3612eb7b6101
Vitaly Peretiatko <viperet@gmail.com>
2025-11-18 12:28:17 +0200

    Add time zone formatting to generated date in footer and enhance date rendering script

--- src/utils/template-engine.ts
+++ src/utils/template-engine.ts
@@ -93,7 +93,9 @@ export class TemplateEngine {
         month: 'long',
         day: 'numeric',
         hour: '2-digit',
-        minute: '2-digit'
+        minute: '2-digit',
+        timeZoneName: 'short',
+        timeZone: 'UTC'
       });
     });
 
